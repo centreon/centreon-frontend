eb897e562f0e1d8c889ac15b4c36898e99b81673
Tom Darneix <tomdar87@outlook.com>
2021-06-21 18:54:32 +0200

    Add a className to popoverMenu

--- packages/centreon-ui/src/PopoverMenu/index.tsx
+++ packages/centreon-ui/src/PopoverMenu/index.tsx
@@ -12,6 +12,7 @@ import { IconButton } from '..';
 
 interface Props {
   children: React.ReactNode;
+  className?: string;
   icon: JSX.Element;
   onClose?: () => void;
   onOpen?: () => void;
@@ -26,6 +27,7 @@ const PopoverMenu = ({
   popperPlacement,
   onOpen,
   onClose,
+  className,
 }: Props): JSX.Element => {
   const theme = useTheme();
   const [anchorEl, setAnchorEl] = React.useState();
@@ -55,7 +57,12 @@ const PopoverMenu = ({
   return (
     <ClickAwayListener onClickAway={close}>
       <div>
-        <IconButton ariaLabel={title} title={title} onClick={toggle}>
+        <IconButton
+          ariaLabel={title}
+          className={className}
+          title={title}
+          onClick={toggle}
+        >
           {icon}
         </IconButton>
         <Popper
