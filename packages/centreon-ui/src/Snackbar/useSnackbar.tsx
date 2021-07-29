909b5fe25314b6e7715045abb8bffe5fbdaa4514
Tom Darneix <tomdar87@outlook.com>
2021-07-29 17:06:29 +0200

    Move snackbars display to notistack

--- packages/centreon-ui/src/Snackbar/useSnackbar.tsx
+++ packages/centreon-ui/src/Snackbar/useSnackbar.tsx
@@ -1,9 +1,59 @@
-import { useContext } from 'react';
+import * as React from 'react';
 
-import { SnackbarContext, SnackbarActions } from './withSnackbar';
+import { useSnackbar as useNotistackSnackbar } from 'notistack';
 
-const useSnackbar = (): SnackbarActions => {
-  return useContext(SnackbarContext);
+import { Typography } from '@material-ui/core';
+
+import Severity from './Severity';
+
+interface ShowMessageProps {
+  message: string | JSX.Element;
+  severity: Severity;
+}
+
+interface ShowMessagesProps {
+  messages: Record<string, string>;
+  severity: Severity;
+}
+
+interface UseSnackbar {
+  showMessage: (props: ShowMessageProps) => void;
+  showMessages: (props: ShowMessagesProps) => void;
+}
+
+const useSnackbar = (): UseSnackbar => {
+  const { enqueueSnackbar } = useNotistackSnackbar();
+
+  const showMessage = ({ message, severity }: ShowMessageProps): void => {
+    enqueueSnackbar({ message, severity });
+  };
+
+  const showMessages = ({ messages, severity }: ShowMessagesProps): void => {
+    const messageKeys = Object.keys(messages);
+
+    const formattedMessages = messageKeys.map(
+      (messageKey) => `${messageKey}: ${messages[messageKey]}`,
+      [],
+    );
+
+    showMessage({
+      message: (
+        <div>
+          {formattedMessages.map((errorMessage, index) => (
+            <Typography key={messageKeys[index]} variant="body2">
+              {errorMessage}
+            </Typography>
+          ))}
+        </div>
+      ),
+      severity,
+    });
+  };
+
+  return {
+    showMessage,
+    showMessages,
+  };
 };
 
 export default useSnackbar;
