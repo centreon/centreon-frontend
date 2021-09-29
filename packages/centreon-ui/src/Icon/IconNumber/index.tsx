66c0e62ddecdc9511f115a517c80d291d3d6c940
Jordan CARO <jcaro@centreon.com>
2021-09-29 17:28:45 +0200

    start of modification to apply the theme to the top counter

--- packages/centreon-ui/src/Icon/IconNumber/index.tsx
+++ packages/centreon-ui/src/Icon/IconNumber/index.tsx
@@ -2,73 +2,76 @@ import React from 'react';
 
 import clsx from 'clsx';
 
-import { makeStyles, Theme, Typography } from '@material-ui/core';
+import { makeStyles, Theme  } from '@material-ui/core/';
 
-const colors = {
-  blue: '#2ad1d4',
-  'gray-dark': '#818185',
-  'gray-light': '#cdcdcd',
-  green: '#87bd23',
-  orange: '#ff9913',
-  red: '#ed1c24',
-};
+import { getStatusColors, Avatar } from '@centreon/ui';
 
-interface StylesProps {
-  iconColor: string;
-}
+import { SeverityCode } from '../../StatusChip';
 
-const useStyles = makeStyles<Theme, StylesProps>((theme) => ({
-  bordered: {
-    border: ({ iconColor }): string => `2px solid ${colors[iconColor]}`,
-  },
-  colored: {
-    background: ({ iconColor }): string => colors[iconColor],
-    border: '2px solid transparent',
-  },
-  icon: {
-    borderRadius: '50px',
-    boxSizing: 'border-box',
-    color: theme.palette.background.paper,
-    cursor: 'pointer',
-    display: 'inline-block',
-    fontSize: '.875rem',
-    height: '32px',
-    margin: '0 6px',
-    minWidth: '32px',
-    overflow: 'hidden',
-    padding: '0px 5px',
-    position: 'relative',
-    textAlign: 'center',
-    textDecoration: 'none',
-  },
-  numberCount: {
-    color: theme.palette.background.paper,
-    lineHeight: '28px',
-  },
-  numberWrap: {
-    fontSize: '0.875rem',
-    position: 'relative',
-    textDecoration: 'none',
-  },
-}));
+const useStyles = makeStyles<Theme, { severityCode?: number }>((theme) => {
+  const getStatusIconColor = (severityCode :string): JSX.Element =>
+    getStatusColors({
+      severityCode,
+      theme,
+    }).backgroundColor;
+
+  return {
+    icon: {
+      borderRadius: '50px',
+      boxSizing: 'border-box',
+      color: theme.palette.background.paper,
+      cursor: 'pointer',
+      display: 'inline-block',
+      fontSize: '.875rem',
+      height: '32px',
+      margin: '0 6px',
+      minWidth: '32px',
+      overflow: 'hidden',
+      padding: '0px 5px',
+      position: 'relative',
+      textAlign: 'center',
+      textDecoration: 'none',
+    },
+    bordered: ({ severityCode }): CreateCSSProperties => ({
+      ...(severityCode && {
+        border: getStatusIconColor(severityCode),
+      }),
+    colored: ({ severityCode }): CreateCSSProperties => ({
+      ...(severityCode && {
+        background: getStatusIconColor(severityCode),
+        border: '2px solid transparent',
+      }),
+    }),
+    numberCount: {
+      color: theme.palette.background.paper,
+      lineHeight: '28px',
+    },
+    numberWrap: {
+      fontSize: '0.875rem',
+      position: 'relative',
+      textDecoration: 'none',
+    },
+  
+})};
 
 interface Props {
-  iconColor: string;
   iconNumber: number | JSX.Element;
   iconType: string;
+  severityCode: SeverityCode;
 }
 
 const IconNumber = ({
-  iconColor,
   iconType,
+  severityCode,
   iconNumber,
 }: Props): JSX.Element => {
-  const classes = useStyles({ iconColor });
+  const classes = useStyles({ severityCode });
 
   return (
     <span className={clsx(classes.icon, classes[iconType], classes.numberWrap)}>
       <span className={classes.numberCount}>
-        <Typography style={{ lineHeight: 'unset' }}>{iconNumber}</Typography>
+        <Avatar colored={{  bgcolor: severityCode  }}>{iconNumber},
+        </Avatar>
       </span>
     </span>
   );
