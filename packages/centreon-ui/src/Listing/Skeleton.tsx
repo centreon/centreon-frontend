6d14c8fa807cc0b6d493ff83d273b7dd04ce5bf1
Tom Darneix <tomdar87@outlook.com>
2021-12-13 17:40:38 +0100

    use text variant

--- packages/centreon-ui/src/Listing/Skeleton.tsx
+++ packages/centreon-ui/src/Listing/Skeleton.tsx
@@ -5,7 +5,7 @@ import LoadingSkeleton from '../LoadingSkeleton';
 const ListingLoadingSkeleton = (): JSX.Element => (
   <>
     {['skeleton1', 'skeleton2', 'skeleton3'].map((key) => (
-      <LoadingSkeleton height={20} key={key} />
+      <LoadingSkeleton height={20} key={key} variant="text" />
     ))}
   </>
 );
