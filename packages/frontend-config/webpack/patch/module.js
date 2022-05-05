d1fd44450ef12c65134cdd8569e7f4b32fef6b31
Tom Darneix <tomdar87@outlook.com>
2022-05-05 16:32:47 +0000

    Remove externals

--- packages/frontend-config/webpack/patch/module.js
+++ packages/frontend-config/webpack/patch/module.js
@@ -1,14 +1,6 @@
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = ({ assetPublicPath, outputPath }) => ({
-  externals: {
-    '@centreon/ui-context': 'CentreonUiContext',
-    jotai: 'Jotai',
-    react: 'React',
-    'react-dom': 'ReactDOM',
-    'react-i18next': 'ReactI18Next',
-    'react-router-dom': 'ReactRouterDom',
-  },
   output: {
     library: '[chunkhash:8]',
     path: outputPath,
