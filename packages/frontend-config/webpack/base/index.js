b7bb74b2daf7ee80f0fd0a58a58cfb88a0e8ff8d
Tom Darneix <tomdar87@outlook.com>
2022-05-17 18:46:56 +0000

    Exports development jsc transform configuration

--- packages/frontend-config/webpack/base/index.js
+++ packages/frontend-config/webpack/base/index.js
@@ -4,70 +4,78 @@ const path = require('path');
 const excludeNodeModulesExceptCentreonUi =
   /node_modules(\\|\/)(?!(centreon-frontend(\\|\/)packages(\\|\/)(ui-context|centreon-ui)))/;
 
-module.exports = (jscTransformConfiguration) => ({
-  cache: false,
-  module: {
-    rules: [
-      {
-        parser: { system: false },
-        test: /\.[cm]?(j|t)sx?$/,
-      },
-      {
-        exclude: excludeNodeModulesExceptCentreonUi,
-        test: /\.[jt]sx?$/,
-        use: {
-          loader: 'swc-loader',
-          options: {
-            jsc: {
-              minify: true,
-              parser: {
-                syntax: 'typescript',
+module.exports = {
+  devJscTransformConfiguration: {
+    react: {
+      development: true,
+      refresh: true,
+    },
+  },
+  getBaseConfiguration: (jscTransformConfiguration) => ({
+    cache: false,
+    module: {
+      rules: [
+        {
+          parser: { system: false },
+          test: /\.[cm]?(j|t)sx?$/,
+        },
+        {
+          exclude: excludeNodeModulesExceptCentreonUi,
+          test: /\.[jt]sx?$/,
+          use: {
+            loader: 'swc-loader',
+            options: {
+              jsc: {
+                minify: true,
+                parser: {
+                  syntax: 'typescript',
+                },
+                transform: jscTransformConfiguration,
               },
-              transform: jscTransformConfiguration,
             },
           },
         },
-      },
-    ],
-  },
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        commons: {
-          chunks: 'initial',
-          filename: '[name].[chunkhash:8].js',
-          minChunks: 2,
-          name: 'commons',
-        },
-        vendor: {
-          chunks: 'initial',
-          enforce: true,
-          filename: '[name].[chunkhash:8].js',
-          name: 'vendor',
-          priority: 10,
-          test: /node_modules/,
-        },
-      },
-      chunks: 'all',
+      ],
     },
-  },
-  output: {
-    chunkFilename: '[name].[chunkhash:8].chunk.js',
-    filename: '[name].[chunkhash:8].js',
-    libraryTarget: 'umd',
-    umdNamedDefine: true,
-  },
-  plugins: [new CleanWebpackPlugin()],
-  resolve: {
-    alias: {
-      '@centreon/ui': path.resolve(
-        './node_modules/centreon-frontend/packages/centreon-ui',
-      ),
-      '@centreon/ui-context': path.resolve(
-        './node_modules/centreon-frontend/packages/ui-context',
-      ),
-      react: path.resolve('./node_modules/react'),
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          commons: {
+            chunks: 'initial',
+            filename: '[name].[chunkhash:8].js',
+            minChunks: 2,
+            name: 'commons',
+          },
+          vendor: {
+            chunks: 'initial',
+            enforce: true,
+            filename: '[name].[chunkhash:8].js',
+            name: 'vendor',
+            priority: 10,
+            test: /node_modules/,
+          },
+        },
+        chunks: 'all',
+      },
     },
-    extensions: ['.js', '.jsx', '.ts', '.tsx'],
-  },
-});
+    output: {
+      chunkFilename: '[name].[chunkhash:8].chunk.js',
+      filename: '[name].[chunkhash:8].js',
+      libraryTarget: 'umd',
+      umdNamedDefine: true,
+    },
+    plugins: [new CleanWebpackPlugin()],
+    resolve: {
+      alias: {
+        '@centreon/ui': path.resolve(
+          './node_modules/centreon-frontend/packages/centreon-ui',
+        ),
+        '@centreon/ui-context': path.resolve(
+          './node_modules/centreon-frontend/packages/ui-context',
+        ),
+        react: path.resolve('./node_modules/react'),
+      },
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    },
+  }),
+};
