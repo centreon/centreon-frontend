1f8b8921406e542168e808ae3a3e59e0fa9c6276
Jordan CARO <jcaro@centreon.com>
2022-05-23 17:36:20 +0200

    Add controls for dialog confirm

--- packages/centreon-ui/src/Dialog/Confirm/index.stories.jsx
+++ packages/centreon-ui/src/Dialog/Confirm/index.stories.jsx
@@ -1,12 +1,22 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+
 import ConfirmDialog from '.';
 
-export default { title: 'Dialog/Confirm' };
+export default { 
+  argTypes: {
+    labelCancel: { control: 'text' },
+    labelConfirm: { control: 'text' },
+    labelMessage: { control: 'text' },
+    labelTitle: { control: 'text' },
+    open: { control: 'boolean' },
+  },
+  component: ConfirmDialog,
+  title: 'Dialog/Confirm',
+} as ComponentMeta<typeof ConfirmDialog>;
 
-export const normal = () => (
-  <ConfirmDialog
-    open
-    labelMessage="Your progress will not be saved."
-    labelTitle="Do you want to confirm action ?"
-    onCancel={() => {}}
-  />
+const TemplateConfirmDialog: ComponentStory<typeof ConfirmDialog> = (args) => (
+  <ConfirmDialog {...args} />
 );
+export const Normal = TemplateConfirmDialog.bind({});
+small.args = { labelMessage: 'Your progress will not be saved.' ,labelTitle:'Do you want to confirm action ?',open: 'true' };
+
