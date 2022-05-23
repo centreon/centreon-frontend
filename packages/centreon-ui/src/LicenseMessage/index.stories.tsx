00a5ca169a0374149dd6bd6b4caf97b4a2be68eb
Jordan CARO <jcaro@centreon.com>
2022-05-23 17:36:50 +0200

    Add controls for  for license message

--- packages/centreon-ui/src/LicenseMessage/index.stories.tsx
+++ packages/centreon-ui/src/LicenseMessage/index.stories.tsx
@@ -1,9 +1,15 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+
 import LicenseMessage from '.';
 
-export default { title: 'License Message' };
+export default {
+  component: LicenseMessage,
+  title: 'License Message',
+} as ComponentMeta<typeof LicenseMessage>;
 
-export const normal = (): JSX.Element => <LicenseMessage />;
+const TemplateLicenseMessage: ComponentStory<typeof LicenseMessage> = (
+  args,
+) => <LicenseMessage {...args} />;
 
-export const withLabel = (): JSX.Element => (
-  <LicenseMessage label="This is a license message" />
-);
+export const withLabel = TemplateLicenseMessage.bind({});
+withLabel.args = { label: 'This is a license message' };
