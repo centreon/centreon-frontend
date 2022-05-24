0a641ceeb4d7f4d19458cdc5ee4b0de852bd3349
Jordan CARO <jcaro@centreon.com>
2022-05-24 09:38:41 +0200

    Add controls for StatusCounter

---
+++ packages/centreon-ui/src/StatusCounter/index.stories.tsx
@@ -0,0 +1,77 @@
+/* eslint-disable react/prop-types */
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+
+import { SeverityCode } from '../StatusChip';
+
+import StatusCounter from '.';
+
+export default {
+  component: StatusCounter,
+  title: 'StatusCounter',
+} as ComponentMeta<typeof StatusCounter>;
+
+const HeaderBackground = ({ children }): JSX.Element => (
+  <div style={{ backgroundColor: '#232f39' }}>{children}</div>
+);
+
+const TemplateStatusCounter: ComponentStory<typeof StatusCounter> = (args) => (
+  <HeaderBackground>
+    <StatusCounter {...args} />
+  </HeaderBackground>
+);
+
+export const customisable = TemplateStatusCounter.bind({});
+
+export const severityCodeHigh = (): JSX.Element => (
+  <HeaderBackground>
+    <StatusCounter count={3} severityCode={SeverityCode.High} />
+  </HeaderBackground>
+);
+
+export const severityCodeMedium = (): JSX.Element => (
+  <HeaderBackground>
+    <StatusCounter count={3} severityCode={SeverityCode.Medium} />
+  </HeaderBackground>
+);
+
+export const severityCodeLow = (): JSX.Element => (
+  <HeaderBackground>
+    <StatusCounter count={3} severityCode={SeverityCode.Low} />
+  </HeaderBackground>
+);
+
+export const severityCodeOk = (): JSX.Element => (
+  <HeaderBackground>
+    <StatusCounter count={3} severityCode={SeverityCode.Ok} />
+  </HeaderBackground>
+);
+
+export const severityCodeHighCount0 = (): JSX.Element => (
+  <HeaderBackground>
+    <StatusCounter count={0} severityCode={SeverityCode.High} />
+  </HeaderBackground>
+);
+
+export const severityCodeMediumCount0 = (): JSX.Element => (
+  <HeaderBackground>
+    <StatusCounter count={0} severityCode={SeverityCode.Medium} />
+  </HeaderBackground>
+);
+
+export const severityCodeLowCount0 = (): JSX.Element => (
+  <HeaderBackground>
+    <StatusCounter count={0} severityCode={SeverityCode.Low} />
+  </HeaderBackground>
+);
+
+export const severityCodeOkCount0 = (): JSX.Element => (
+  <HeaderBackground>
+    <StatusCounter count={0} severityCode={SeverityCode.Ok} />
+  </HeaderBackground>
+);
+
+export const severityCodeOkBigCount = (): JSX.Element => (
+  <HeaderBackground>
+    <StatusCounter count={500000} severityCode={SeverityCode.Ok} />
+  </HeaderBackground>
+);
