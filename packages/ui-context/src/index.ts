293bb9f63e91a72f0b5f6dae895a82d1878fe263
Jordan CARO <jcaro@centreon.com>
2022-06-06 16:15:39 +0200

    fix eslint issue

--- packages/ui-context/src/index.ts
+++ packages/ui-context/src/index.ts
@@ -4,7 +4,7 @@ export { default as downtimeAtom } from './downtimeAtom';
 export { default as refreshIntervalAtom } from './refreshIntervalAtom';
 export { default as cloudServicesAtom } from './cloudServicesAtom';
 export { default as acknowledgementAtom } from './acknowledgementAtom';
-export { default as statusOptimizedModeAtom } from './statusOptimizedModeAtom';
+export { default as defaultResourceStorageOptimizationMode } from './resourceStorageOptimizationMode';
 export { ThemeMode } from './types';
 
 export type {
