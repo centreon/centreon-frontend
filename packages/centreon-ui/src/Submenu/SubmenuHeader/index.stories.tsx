1f325bbc8a9ece95ae3e789076fec6ea7e04bf03
Jordan CARO <jcaro@centreon.com>
2022-06-09 16:03:49 +0200

    Add SubmenuHeader to Controls

--- packages/centreon-ui/src/Submenu/SubmenuHeader/index.stories.tsx
+++ packages/centreon-ui/src/Submenu/SubmenuHeader/index.stories.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+
 import HostIcon from '@mui/icons-material/Dns';
 
 import { IconHeader, StatusCounter, IconToggleSubmenu } from '../..';
@@ -10,7 +12,13 @@ import SubmenuItem from './SubmenuItem';
 
 import SubmenuHeader from '.';
 
-export default { title: 'SubemnuHeader' };
+export default {
+  argTypes: {
+    active: { control: 'boolean' },
+  },
+  component: SubmenuHeader,
+  title: 'SubmenuHeader',
+} as ComponentMeta<typeof SubmenuHeader>;
 
 const Submenu = (): JSX.Element => {
   const [active, setActive] = useState(false);
@@ -73,5 +81,11 @@ const Submenu = (): JSX.Element => {
     </div>
   );
 };
+
+const TemplateSubmenuHeader: ComponentStory<typeof SubmenuHeader> = (args) => (
+  <Submenu active {...args} />
+);
+
+export const DynamicStatusChip = TemplateSubmenuHeader.bind({});
 
 export const hostSubmenu = (): JSX.Element => <Submenu />;
