2e44746809c1866bb2c2f8de58ba9bb1b75e9203
nouha <nelabrouki-ext@centreon.com>
2022-07-05 11:33:06 +0000

    test

--- packages/centreon-ui/src/InputField/Select/Autocomplete/Connected/index.tsx
+++ packages/centreon-ui/src/InputField/Select/Autocomplete/Connected/index.tsx
@@ -41,8 +41,7 @@ const ConnectedAutocompleteField = (
     open,
     conditionField = 'id',
     searchConditions = [],
-    getRenderedOptionText = (option): string =>
-      option[column]?.toString() as string,
+    getRenderedOptionText = (option): string => option.name,
     getRequestHeaders,
     displayOptionThumbnail,
     ...props
